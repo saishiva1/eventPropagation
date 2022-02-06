c3208bc23e0786393f70f5cd8ea95d0cf7cf6012
saishiva1 <42500620+saishiva1@users.noreply.github.com>
2022-02-06 22:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-console.log('hello')
+console.log('hello');
 (function () {
-    console.log('deferred')
-    const resetBtn = document.querySelector('.btn-reset')
+    console.log('deferred');
+    const resetBtn = document.querySelector('.btn-reset');
     //removed , '#btn-opt' from 2 arrays
-    const eleArray = ['.p1', '.p2', '.p3', '.btn', '#p1-opt', '#p2-opt', '#p3-opt']
+    const eleArray = ['.p1', '.p2', '.p3', '.btn', '#p1-opt', '#p2-opt', '#p3-opt'];
     const optionsList = ['#p1-opt', '#p2-opt', '#p3-opt'];
-    let num = 1
+    let num = 1;
 
     optionsList.forEach(eleId => {
-        const element = document.querySelector(eleId)
-        element.checked = false
+        const element = document.querySelector(eleId);
+        element.checked = false;
     })
 
 //start of new code
@@ -27,7 +27,7 @@ console.log('hello')
 
         function onClickCheckBox(e) {
             // console.log(e, this)
-            const eleToBeChanged = document.querySelector(`.${this.name}`)
+            const eleToBeChanged = document.querySelector(`.${this.name}`);
             if (e.target.checked) {
                 eleToBeChanged.removeEventListener('click', onClickAction, false);
                 eleToBeChanged.addEventListener('click', onClickAction, true);
@@ -39,8 +39,8 @@ console.log('hello')
 
         function onClickAction(e) {
                 e.preventDefault();
-                console.log(e.cancelable)
-                addColor(this)
+                //console.log(e.cancelable)
+                addColor(this);
         }
 
         resetBtn.addEventListener('click', () => {
@@ -72,45 +72,45 @@ console.log('hello')
 
     function resetColors() {
         for (let i = 0; i < 4; i++) {
-            const element = document.querySelector(eleArray[i])
+            const element = document.querySelector(eleArray[i]);
             element.style.backgroundColor = i == 3 ? '#9e33ff' : 'azure'
         }
-        num = 1
+        num = 1;
     }
 
     function resetPlayground() {
-        resetColors()
-        resetList()
+        resetColors();
+        resetList();
     }
 
     function addColor(element) {
-        console.log(num, 'start')
-        resetBtn.disabled = true
-        resetList()
+        console.log(num, 'start');
+        resetBtn.disabled = true;
+        resetList();
 
-        let color = ''
+        let color = '';
         if (num == 1) {
-            color = '#ff4545'
-            resetColors()
+            color = '#ff4545';
+            resetColors();
         }
         if (num == 2) {
-            color = '#00bd94'
+            color = '#00bd94';
         }
         if (num == 3) {
-            color = '#c7b600'
+            color = '#c7b600';
         }
         if (num == 4) {
-            color = '#fc8403'
+            color = '#fc8403';
         }
 
         setTimeout((num, color) => {
-            element.style.backgroundColor = color
-            addChild(element.classList[0].toString())
+            element.style.backgroundColor = color;
+            addChild(element.classList[0].toString());
             if (num === 4) {
-                resetBtn.disabled = false
+                resetBtn.disabled = false;
             }
-        }, num * 500, num, color)
+        }, num * 500, num, color);
 
-        num = num === 4 ? 1 : ++num
+        num = num === 4 ? 1 : ++num;
     }
 })()
